a0a5a5c2b8f40c4b72b8a153101b6c4a63ca1158
IbrahimSouhal <souhalibrahim2010@gmail.com>
2026-05-22 16:58:56 +0100

    Add lesson6 - Strings and update README

---
+++ lesson6.py
@@ -0,0 +1,9 @@
+name = "Ibrahim"
+city = "Beni Mellal"
+dream = "Robotic Engineer"
+
+message = "My name is " + name + ", I am from " + city + " and my dream is to become a " + dream + "."
+print(message)
+
+message2 = f"My name is {name}, I am from {city} and my dream is to become a {dream}."
+print(message2)
