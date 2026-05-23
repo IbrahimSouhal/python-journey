ee9ab3b11d8bbb51f6754199e35de412d1fa0f21
IbrahimSouhal <souhalibrahim2010@gmail.com>
2026-05-23 12:50:22 +0100

    Add project3 - Calculator

---
+++ project3.py
@@ -0,0 +1,22 @@
+def calculate(num1, operator, num2):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        return "Invalid operator"
+    while True:
+        num1 = float(input("Enter the first number: "))
+        operator = input("Enter the operator (+, -, *, /): ")
+        num2 = float(input("Enter the second number: "))
+
+        result = calculate(num1, operator, num2)
+        print(f"The result is: {result}")
+
+        again = input("Calculate again? (yes/no): ")
+        if again == "no":
+            break
