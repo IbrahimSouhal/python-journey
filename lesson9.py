1e8f920fac55ef2ccccfe6846b248952bcfab3e9
IbrahimSouhal <souhalibrahim2010@gmail.com>
2026-05-23 17:36:34 +0100

    Update lesson9 - Add move and stop methods

--- lesson9.py
+++ lesson9.py
@@ -5,9 +5,17 @@ class  robot:
 
     def introduce (self):
         print(f"My name is {self.name} and my speed is {self.speed} km/h.")
+    def move(self):
+        print(f"{self.name} is moving at {self.speed} km/h.")
+
+    def stop(self):
+        print(f"{self.name} has stopped.")
 
 robot1 = robot("Atlas", 10)
 robot2 = robot("Spot", 5)
 
 robot1.introduce()
 robot2.introduce()
+
+robot1.move()
+robot1.stop()
